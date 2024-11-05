54c4173255dca95289dce0a67cf6e9af51ec9482
cvarela3344 <claudio.varela@usm.cl>
2024-11-04 22:39:31 -0300

    v2

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
-import '../stylesheets/Test/cards.scss'; // Asegúrate de que este archivo contenga tu código SCSS de las tarjetas
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Card = ({ text1, text2 }) => {
+const Card = ({ text1, text2, text3, link }) => {
   return (
     <div className="card">
-      <div className="first-content">
-        <h2>{text1}</h2>
-      </div>
-      <div className="second-content">
-        <h2>{text2}</h2>
-      </div>
+      <div className="first-content"><h2>{text1}</h2></div>
+      <div className="second-content"><p>{text2}</p></div>
+      <Link to={link} className="card-link">
+        {text3}
+      </Link>
     </div>
   );
+};
+
+Card.propTypes = {
+  text1: PropTypes.string.isRequired,
+  text2: PropTypes.string.isRequired,
+  text3: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
 };
 
 export default Card;
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,21 +1,21 @@
-
 import React from 'react';
-import Card from '../components/card'; // Asegúrate de que el camino de importación sea correcto
-import '../stylesheets/Test/cards.scss'
+import Card from '../components/card';
+import '../stylesheets/Test/cards.scss';
+
 export const Tests = () => {
   const cardsData = [
-    { text1: 'Test1', text2: 'Texto 2 - Card 1' },
-    { text1: 'Test2', text2: 'Texto 2 - Card 2' },
-    { text1: 'Texto 1 - Card 3', text2: 'Texto 2 - Card 3' },
-    { text1: 'Texto 1 - Card 4', text2: 'Texto 2 - Card 4' },
-    { text1: 'Texto 1 - Card 5', text2: 'Texto 2 - Card 5' },
-    { text1: 'Texto 1 - Card 6', text2: 'Texto 2 - Card 6' },
+    { text1: 'Test1', text2: 'Este test sirve para bla bla bla papapapa', text3: 'Ver más', link: '/detalles/1' },
+    { text1: 'Test2', text2: 'Texto 2 - Card 2', text3: 'Ver más', link: '/detalles/2' },
+    { text1: 'Texto 1 - Card 3', text2: 'Texto 2 - Card 3', text3: 'Ver más', link: '/detalles/3' },
+    { text1: 'Texto 1 - Card 4', text2: 'Texto 2 - Card 4', text3: 'Ver más', link: '/detalles/4' },
+    { text1: 'Texto 1 - Card 5', text2: 'Texto 2 - Card 5', text3: 'Ver más', link: '/detalles/5' },
+    { text1: 'Texto 1 - Card 6', text2: 'Texto 2 - Card 6', text3: 'Ver más', link: '/detalles/6' },
   ];
 
   return (
     <div className="cards-container">
       {cardsData.map((card, index) => (
-        <Card key={index} text1={card.text1} text2={card.text2} />
+        <Card key={index} text1={card.text1} text2={card.text2} text3={card.text3} link={card.link} />
       ))}
     </div>
   );
